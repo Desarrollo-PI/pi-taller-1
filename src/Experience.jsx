73383fdbf438485f7df131c33e514e4be24b54cc
Samuel Galindo <itzsaky@outlook.com>
2024-03-05 20:19:13 -0500

    piso 100 x 10

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -4,6 +4,7 @@ import { useRef, useState } from "react";
 
 const Experience = () => {
 	const boxRef = useRef(null);
+	const groundRef = useRef(null);
 	const [y, setY] = useState(0);
 
 	useFrame((state, delta) => {
@@ -20,6 +21,10 @@ const Experience = () => {
 			<mesh ref = {boxRef}>
 				<boxGeometry args = {[1,1,1]} />
 				<meshStandardMaterial color = "MediumTurquoise" />
+			</mesh>
+			<mesh ref = {groundRef} position={[0,-2, 0]}>
+				<boxGeometry args = {[10,0.5,100]} />
+				<meshStandardMaterial color={[159/255, 159/255, 159/255]}/>
 			</mesh>
 		</>
 	)
